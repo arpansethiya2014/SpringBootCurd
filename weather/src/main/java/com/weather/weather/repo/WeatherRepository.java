42dccc85a765e02ed2304f6bd2e37ce487872927
arpansethiya78699 <arpansethiya2014@gmail.com>
2020-09-26 23:29:39 +0530

    Create query for search By city

--- weather/src/main/java/com/weather/weather/repo/WeatherRepository.java
+++ weather/src/main/java/com/weather/weather/repo/WeatherRepository.java
@@ -1,6 +1,9 @@
 package com.weather.weather.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.weather.weather.entity.Weather;
@@ -8,4 +11,7 @@ import com.weather.weather.entity.Weather;
 @Repository
 public interface WeatherRepository extends JpaRepository<Weather, Long> {
 
+	@Query(value = "SELECT * FROM Weather w WHERE w.city = ?",nativeQuery = true)
+	Optional<Weather> findByCity(String city);
+	
 }
